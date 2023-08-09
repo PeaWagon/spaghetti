d8927b0c67ae4c6a3a6358b7f18695dba19754cf
Jen <25353074+PeaWagon@users.noreply.github.com>
2023-08-09 16:53:05 -0400

    add models for customer and product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(unique=True, max_length=64)
+    phone = models.BigIntegerField(
+        validators=[
+            MinValueValidator(limit_value=1),
+            MaxValueValidator(limit_value=int("9" * 15)),
+        ]
+    )
+    birth_date = models.DateField(null=True)
